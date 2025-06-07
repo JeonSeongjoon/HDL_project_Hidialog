75c098ccce389d1422616f6abaab6405e3ab5c16
성준 전 <02seongjoom@gmail.com>
2025-06-07 07:53:40 +0000

    initial commit

---
+++ exp_dialogre.sh
@@ -0,0 +1,3 @@
+export RoBERTa_LARGE_DIR=/teamspace/studios/this_studio/HiDialog/pre-trained_model/RoBERTa
+
+nohup python /teamspace/studios/this_studio/HiDialog/run_classifier.py --do_train --do_eval --encoder_type RoBERTa  --data_dir /teamspace/studios/this_studio/HiDialog/datasets/DialogRE --data_name DialogRE   --vocab_file $RoBERTa_LARGE_DIR/vocab.json --merges_file $RoBERTa_LARGE_DIR/merges.txt  --config_file $RoBERTa_LARGE_DIR/config.json   --init_checkpoint $RoBERTa_LARGE_DIR/pytorch_model.bin   --max_seq_length 512   --train_batch_size 12   --learning_rate 7.5e-6   --num_train_epochs 30  --output_dir DialogRE --gradient_accumulation_steps 2 --gpu 0 > /teamspace/studios/this_studio/HiDialog/logs/DialogRE.log 2>&1 &
